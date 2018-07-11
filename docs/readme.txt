a31c76b92663a70cf4a1bc0ebce68c3444189cf4
bilbobagg1ns <mprasad@regis.edu>
2018-07-11 09:52:04 -0600

    Genesis

---
+++ docs/readme.txt
@@ -0,0 +1,8 @@
+1. Make sure you are done with MySQL tutorial of installing MySQL and configuring it.
+
+2. Unzip the mysql-connector-java-3.1.13.zip and extract and copy JDBC driver jar
+   - mysql-connector-java-3.1.13-bin.jar into the lib folder of this example.
+   
+3. Edit mysetenv.cmd and make the relavant updates.
+
+4. Compile and Run.
---
+++ src/JDBConnectionPoolSimpleSample.java
@@ -0,0 +1,200 @@
+/*-
+
+ADVANCED TOPIC
+
+Creating and destroying database connections is expensive. In the real world, 
+one uses connections pools to pre-create connections and the applications draws
+connections from this connection pool and releases them back into the pool after
+each use. This mechanism is far less taxing on the application performance than
+creating them afresh on demand.
+
+In the web application world, the application server (Tomcat, Jetty, JBoss etc.)
+at start up time are configured to startup with a connection pool.
+
+There are number of connections pools (dbcp2, tomcat, vilbur, hikari). For
+our exploration, we'll use a connection pool called C3P0.
+
+This sample demonstrates how to use this connection pool. Usage is extremely 
+simple.
+
+At a high-level, here are the steps:
+===================================
+Create C3P0 datasource/connection pool
+   - Fetch connection from this pool
+   - After use, release connection back into the pool (DO NOT CLOSE DATASOURCE/POOL HERE)
+When application is ready to be shutdown
+   - Now CLOSE the DATASOURCE and the associated connection pool
+
+Download and Sample Usage:
+==========================
+1. Download C3P0 from http://sourceforge.net/projects/c3p0/
+    - Unzip contents
+    - Copy jars into into your lib folder (this sample already has these libs)
+				- Read the documentation and explore the examples for further knowledge
+ 
+2. Start MySql database
+   - Confirm by logging into it via command line or using MySql Workbench UI
+ 
+3. Compile and Run JDBConnectionPoolSimpleSample.java
+
+NOTE: If you are using ant, then  you need to set the property correctly in 
+   build.xml file.
+
+@author Mike Prasad
+ */
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.mchange.v2.c3p0.DataSources;
+import java.beans.PropertyVetoException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBConnectionPoolSimpleSample {
+
+  private static final int MINPOOLSIZE = 5;
+  private static final int MAXPOOLSIZE = 10;
+
+  private static ComboPooledDataSource cpds = new ComboPooledDataSource();
+
+  public JDBConnectionPoolSimpleSample() {
+  }
+
+  /**
+   * Create the pool.
+   *
+   * As always, in real world we need to get these hard coded values from a
+   * property file
+   */
+  public static void createPool() {
+    System.out.println("-Creating Connection Pool");
+    try {
+      cpds = new ComboPooledDataSource();
+      cpds.setDriverClass("com.mysql.jdbc.Driver"); //loads driver
+      cpds.setJdbcUrl("jdbc:mysql://localhost:3306/mysql");
+      cpds.setUser("root");
+      cpds.setPassword("root");
+      cpds.setMaxPoolSize(MAXPOOLSIZE);
+      cpds.setMinPoolSize(MINPOOLSIZE);
+    } catch (PropertyVetoException pve) {
+      //replace with log4 logging
+      //log.error  (sqe.getClass()+": "+ sqe.getMessage(), sqe);	
+      System.out.println(pve.getClass() + ": " + pve.getMessage());
+      StringWriter errors;
+      pve.printStackTrace(new PrintWriter(errors = new StringWriter()));
+      System.out.println(errors.toString());
+    }
+    System.out.println("-Connection Pool creation completed");
+  }
+
+  /**
+   * Retrieves a connection from the connection pool
+   *
+   * @return Connection if available or returns null
+   */
+  public static Connection getConnection() {
+    Connection connection = null;
+    try {
+      connection = cpds.getConnection();
+    } catch (SQLException sqe) {
+      //replace with log4 logging
+      //log.error  (sqe.getClass()+": "+ sqe.getMessage(), sqe);	
+      System.out.println(sqe.getClass() + ": " + sqe.getMessage());
+      StringWriter errors;
+      sqe.printStackTrace(new PrintWriter(errors = new StringWriter()));
+      System.out.println(errors.toString());
+    }
+    return connection;
+  }
+
+  /**
+   * We want to close the DataSource *only* when the application is ready to be
+   * shutdown.
+   */
+  public static void shutDown() {
+    try {
+      if (cpds != null) {
+        System.out.println("Destroying datasource");
+        DataSources.destroy(cpds);
+      } //release datasource         
+    } catch (SQLException e) {
+      //replace with log4 logging
+      //log.error  (sqe.getClass()+": "+ sqe.getMessage(), sqe);	
+      System.out.println(e.getClass() + ": " + e.getMessage());
+      StringWriter errors;
+      e.printStackTrace(new PrintWriter(errors = new StringWriter()));
+      System.out.println(errors.toString());
+    }
+  } //end shutDown
+
+  /**
+   * Fetches connection from database and lists contents of a USER table
+   */
+  public void listContents() {
+    Statement stmt = null;
+    Connection conn = null;
+
+    try {
+      //fetch db connection
+      conn = getConnection();
+
+      if (conn != null) //if we have a connection
+      {
+        stmt = conn.createStatement();
+
+        // Select the columns from the USER table.
+        ResultSet rset = stmt.executeQuery("select HOST, USER from USER");
+
+        // Iterate through the result set and print its contents.
+        System.out.println("\nResult set returned: ");
+
+        while (rset.next()) {
+          System.out.println("Host: " + rset.getString(1));
+          System.out.println("User: " + rset.getString(2));
+        }
+      } else {
+        System.out.println("Issue during fetching connection.");
+      }
+    } catch (SQLException e) {
+      //replace with log4 logging
+      //log.error  (sqe.getClass()+": "+ sqe.getMessage(), sqe);	
+      System.out.println(e.getClass() + ": " + e.getMessage());
+      StringWriter errors;
+      e.printStackTrace(new PrintWriter(errors = new StringWriter()));
+      System.out.println(errors.toString());
+    } finally //must release all open resources in finally block
+    {
+      try {
+        // check for null first before closing resources
+        if (stmt != null) {
+          stmt.close();
+        }
+        if (conn != null) {
+        }
+        DataSources.destroy(cpds);
+
+      } catch (SQLException e) {
+        //replace with log4 logging
+        //log.error  (sqe.getClass()+": "+ sqe.getMessage(), sqe);	
+        System.out.println(e.getClass() + ": " + e.getMessage());
+        StringWriter errors;
+        e.printStackTrace(new PrintWriter(errors = new StringWriter()));
+        System.out.println(errors.toString());
+      }
+    }//try/catch/finally     
+  } // end listContents()
+
+  public static void main(String[] args) {
+    JDBConnectionPoolSimpleSample sample = new JDBConnectionPoolSimpleSample();
+    JDBConnectionPoolSimpleSample.createPool();
+    sample.listContents();
+
+    System.out.println("-Shutting down application");
+    JDBConnectionPoolSimpleSample.shutDown();
+
+  }//end main
+
+}//end JDBConnectionPoolSimpleSample
